1cc363f78e22b42a40c2ed2353c2015c77ab9725
Kevin Stradtman <stradtkt@gmail.com>
2018-11-14 21:40:57 -0500

    Added some changes to the cart

--- carts/models.py
+++ carts/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.conf import settings
 from products.models import Product
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 User = settings.AUTH_USER_MODEL
 
 
+
 class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = self.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
         user_obj = None
         if user is not None:
@@ -22,3 +37,15 @@ class Cart(models.Model):
     objects = CartManager()
     def __str__(self):
         return str(self.id)
+
+
+def pre_save_cart_reciever(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        products = instance.products.all()
+        total = 0
+        for p in products:
+            total += p.price
+        instance.total = total
+        instance.save()
+
+m2m_changed.connect(pre_save_cart_reciever, sender=Cart.products.through)
--- carts/views.py
+++ carts/views.py
@@ -7,13 +7,9 @@ def cart_create(user=None):
     return cart_obj
 
 def carts_home(request):
-    # cart_id = request.session.get("cart_id", None)
-    # if cart_id is None:
-    #     request.session['cart_id'] = cart_obj.id
-    # else:
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj.first()
-    else:
-        cart_obj = Cart.objects.new(user=None)
-    return render(request, "carts/index.html", {})
+    cart_obj = Cart.objects.new_or_get(request)
+    
+    context = {
+        "cart": cart_obj
+    }
+    return render(request, "carts/index.html", context)
